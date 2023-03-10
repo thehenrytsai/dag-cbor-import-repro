bcfb58d2e2e1a26a0598a2cd626ed9c3ce5f0884
Henry Tsai <henrytsai@squareup.com>
2023-03-09 17:52:37 -0800

    showing import issue for ipfs-unixfs-exporter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import * as cbor from '@ipld/dag-cbor';
+import { exporter } from 'ipfs-unixfs-exporter';
 
-// a map of all support codecs.This map is used to select the appropriate codec
-// when generating a CID to compare against a provided CID
-const codecs = {
-  [cbor.code]: cbor
-};
